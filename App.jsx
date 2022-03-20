1ea8ab6a18b8710d44b2b5b4b96b384e55d12a8a
Sh Tanmoy <62763494+tanmoy108@users.noreply.github.com>
2022-03-20 23:29:50 +0600

    component 20/3/2022 11:29PM

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./Heading";
+import Title from "./Title";
+import Order from "./Order";
+
+function App() {
+  return (
+    <>
+      <Title />
+      <Heading />
+      <Order />
+    </>
+  );
+}
+
+export default App;
---
+++ Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heading() {
+  return <p>top 5 zoom movies i have seen</p>;
+}
+
+export default Heading;
