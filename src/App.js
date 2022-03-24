0edb795b27ca13fc025a6c08bb3a50675b9ee8b7
shtanmoy <tanmoysharma46@gmail.com>
2022-03-24 23:00:17 +0600

    increament decreament 24/3/2022 11:00 PM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <></>;
+  const state = useState();
+  const [count, update] = useState(0);
+
+  const ButtonClick1 = () => {
+    update(count+1);
+  };
+
+  const ButtonClick2 = () => {
+    update(count - 1);
+  }
+
+  return (
+    <>
+      <div className="part">
+        <h1>{count}</h1>
+        <div style={{ textAlign: "center" }}>
+          <button onClick={ButtonClick1}>Increament</button>
+          <button onClick={ButtonClick2}>Decreament</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
