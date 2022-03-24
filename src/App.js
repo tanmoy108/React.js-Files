c28d86c186ccadf9970b1c11ef2260a414db2444
shtanmoy <tanmoysharma46@gmail.com>
2022-03-24 21:50:54 +0600

    commit 24/3/2022 9:50PM

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from "react";
-import Movie from "./Movie";
-import List from "./Listdb";
 
 const App = () => (
   <>
-    {List.map((value) => {
-      return (
-        <Movie
-          key={value.id}
-          imgSrc={value.img}
-          title={value.name}
-          website={value.web}
-          about={value.aboutMovie}
-          link={value.mlink}
-        />
-      );
-    })}
+
   </>
 );
 
