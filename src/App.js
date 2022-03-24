a39e7757d61c3eda62fb0f2c0de9de02c89f61a3
shtanmoy <tanmoysharma46@gmail.com>
2022-03-24 14:10:30 +0600

    fat arrow function & key update 24/3/2022 2:10PM

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from "react";
 import Movie from "./Movie";
 import List from "./Listdb";
 
-function getMovie(value) {
-  return (
-    <Movie
-      imgSrc={value.img}
-      title={value.name}
-      website={value.web}
-      about={value.aboutMovie}
-      link={value.mlink}
-    />
-  );
-}
-
-function App() {
-  return <>{List.map(getMovie)}</>;
-}
+const App = () => (
+  <>
+    {List.map((value) => {
+      return (
+        <Movie
+          key={value.id}
+          imgSrc={value.img}
+          title={value.name}
+          website={value.web}
+          about={value.aboutMovie}
+          link={value.mlink}
+        />
+      );
+    })}
+  </>
+);
 
 export default App;
--- src/Listdb.jsx
+++ src/Listdb.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Listdb = [
   {
+    id:1,
     img: "https://www.cafehallyu.com/wp-content/uploads/2022/02/61cabe1bdc381.png",
     name: "All of us are Dead",
     web:"NetFlix",
@@ -10,6 +11,7 @@ const Listdb = [
     mlink: "https://www.netflix.com/bd/title/81237994",
   },
   {
+    id:2,
     img: "https://1739752386.rsc.cdn77.org/data/images/full/241016/sweet-home.jpeg",
     name: "A sweet home",
     web:"NetFlix",
@@ -18,6 +20,7 @@ const Listdb = [
     mlink: "https://www.netflix.com/bd/title/81061734",
   },
   {
+    id:3,
     img: "https://www.nme.com/wp-content/uploads/2020/03/train-to-busan.jpg",
     name: "Train To Busan",
     web:"Prime Video",
@@ -26,6 +29,7 @@ const Listdb = [
     mlink: "https://www.amazon.com/Train-Busan-Gong-Yoo/dp/B0861KTPPS",
   },
   {
+    id:4,
     img: "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABZ0YASBbNXtrybLoLlCp4ktnzkk2DpGGInuM_7I21jdSkENpyTVuKC71CzV6AnSXB5kIc7Y4Tz4Rgl__dII9Q1NMyg4j.jpg?r=d62",
     name: "Alive",
     web:"NetFlix",
@@ -34,6 +38,7 @@ const Listdb = [
     mlink: "https://www.netflix.com/bd/title/81240831",
   },
   {
+    id:5,
     img: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/17245eb21b86f69ac3602b279fb76e9b8fd66461538c936135bff47ec0d17602._UY500_UX667_RI_V_TTW_.jpg",
     name: "World War Z",
     web:"Prime Video",
@@ -42,6 +47,7 @@ const Listdb = [
     mlink: "https://www.amazon.com/World-War-Z-Brad-Pitt/dp/B00EL8I8IS",
   },
   {
+    id:6,
     img: "https://cdn0-production-images-kly.akamaized.net/HDIphBvzM0LvmtUilHQiBvQi8HM=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3636438/original/056651500_1637212041-Premium_Headline_Mobile_Peninsula.jpg",
     name: "Peninsula",
     web:"Prime Video",
