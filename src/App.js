bbeadcd03c2d787ef5a55b7a3d58a8847e75a72d
shtanmoy <tanmoysharma46@gmail.com>
2022-03-25 13:52:56 +0600

    digital clock 25/3/2022 1:52 PM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <></>;
+  let time = new Date().toLocaleTimeString();
+  const [count, update] = useState(time);
+
+  const GetTime = () => {
+    time = new Date().toLocaleTimeString();
+    update(time);
+  };
+
+  setInterval(GetTime,1000);
+  return (
+    <>
+      <div className="part">
+        <h1>{count}</h1>
+      </div>
+    </>
+  );
 };
 
 export default App;
