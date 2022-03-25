d0778db5b64d9dcbb3af2d692fe0f9e0c5896d8b
shtanmoy <tanmoysharma46@gmail.com>
2022-03-25 19:55:29 +0600

    input form by hook and event 25/3/2022 7:55PM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <></>;
+  const [current, update] = useState("");
+  const [a, b] = useState();
+  const [current2, update2] = useState("");
+  const [c, d] = useState();
+
+  const TypeName = (e) => {
+    let x = e.target.value;
+    update(x);
+  };
+
+  const TypeName1 = (event) => {
+    event.preventDefault();
+    b(current);
+    d(current2);
+  };
+
+  const TypeLName = (e) => {
+    update2(e.target.value);
+  };
+  return (
+    <>
+      <div className="part">
+        <form onSubmit={TypeName1}>
+          <h1>
+            Hello {a}
+            {c}
+          </h1>
+          <input
+            type="text"
+            placeholder="Enter Your First Name"
+            onChange={TypeName}
+            value={current}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Enter Your Last Name"
+            onChange={TypeLName}
+            value={current2}
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default App;
