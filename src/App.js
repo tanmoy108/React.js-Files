a819eea10a456d0085e32ecb2a2a9e37dfd386cc
shtanmoy <tanmoysharma46@gmail.com>
2022-03-25 14:38:15 +0600

    mouse event 25/2/2022 2:38PM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <></>;
+  const [currentColor, updateColor] = useState("thistle");
+  const [currentText, updateText] = useState("Click Me");
+
+  const Clickone = () => {
+    updateColor("rgb(114, 7, 255)");
+    updateText("What ?? 😠 ");
+  };
+
+  const Mousetwice = () => {
+    updateColor("rgb(253, 137, 137)");
+    updateText("Hey !! 👊 ");
+  };
+  const Mouseout = () => {
+    updateColor("thistle");
+    updateText("Click Me");
+  };
+
+  return (
+    <>
+      <div className="part" style={{ backgroundColor: currentColor }}>
+        <button
+          onClick={Clickone}
+          onMouseOver={Mousetwice}
+          onMouseOut={Mouseout}
+        >
+          {currentText}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default App;
