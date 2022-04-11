16ba7acd645d11f822350cd42666cf2af9460910
shtanmoy <tanmoysharma46@gmail.com>
2022-04-12 01:08:50 +0600

    12/4/2022 1:08pm

--- src/Apage.jsx
+++ src/Apage.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Bpage from "./Bpage";
 
 const Apage = () => {
   return (
     <>
-      <Bpage />
+     
     </>
   );
 };
--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-import React, { createContext } from "react";
-import Apage from "./Apage";
-
-const Name = createContext();
-const Lastname = createContext();
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [current, update] = useState(0);
+  const [c, u] = useState([{}]);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await axios.get(
+        `https://pokeapi.co/api/v2/berry/${current}/`
+      );
+      //console.log(res.data.flavors);
+     u(()=>{
+       return[res.data.flavors]
+     });
+     
+    }
+    getData();
+  },[current]);
+
+  console.log(c[0][0]);
+
   return (
     <>
-      <Name.Provider value="Tanmoy">
-        <Lastname.Provider value="Sharma">
-          <Apage />
-        </Lastname.Provider>
-      </Name.Provider>
+      <h1>{current}</h1>
+
+      <select
+        value={current}
+        onChange={(e) => {
+          update(e.target.value)
+        }}
+      >
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+      </select>
+     
     </>
   );
 };
 
 export default App;
-export { Name, Lastname };
