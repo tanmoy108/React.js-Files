2f4576d48f76f47c875e9053ba6bb2da0a6d51a9
shtanmoy <tanmoysharma46@gmail.com>
2022-04-13 01:34:47 +0600

    13/4/2022 1:34AM

--- src/Cpage.jsx
+++ src/Cpage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams,useNavigate } from "react-router-dom";
 
 const Cpage = () => {
   const { fname, lname } = useParams();
   const location = useLocation();
+  const history = useNavigate();
   console.log(location);
   return (
     <>
@@ -15,13 +16,22 @@ const Cpage = () => {
       <h2>path: {location.pathname}</h2>
 
       {location.pathname === "/cpage/tanmoy/sharma" ? (
-        <button
+       <>
+       <button
           onClick={() => {
             alert("you got it");
           }}
         >
           hello
         </button>
+        <button onClick={()=>{history(-1)}} >Go Back</button>
+        <button onClick={()=>{history(-2)}} >Go 2 Back</button>
+        <button onClick={()=>{history(1)}} >Go Forward</button>
+        <button onClick={()=>{history(2)}} >Go 2 Forward</button>
+        <button onClick={()=>{history('/')}} >home</button>
+        <button onClick={()=>{history('/dpage')}} >Dpage</button>
+       </>
+
       ) : null}
     </>
   );
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -5,24 +5,28 @@ const Menu = () => {
   return (
     <>
       <NavLink
+        style={{ fontSize: "30px",textDecoration: "none" }}
         className={({ isActive }) => (isActive ? "active-style" : "none")}
         to="/"
       >
         Apage
       </NavLink>
       <NavLink
+        style={{ fontSize: "30px",textDecoration: "none" }}
         className={({ isActive }) => (isActive ? "active-style" : "none")}
         to="/bpage"
       >
         Bpage
       </NavLink>
       <NavLink
+        style={{ fontSize: "30px",textDecoration: "none" }}
         className={({ isActive }) => (isActive ? "active-style" : "none")}
         to="/cpage/tanmoy/sharma"
       >
         Cpage
       </NavLink>
       <NavLink
+        style={{ fontSize: "30px",textDecoration: "none" }}
         className={({ isActive }) => (isActive ? "active-style" : "none")}
         to="/dpage"
       >
